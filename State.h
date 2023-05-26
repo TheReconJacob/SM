12e3574d1cb39461e03e68f773651c75385eda67
thereconjacob <jvpokeyjake@gmail.com>
2023-05-26 10:17:08 +0100

    define state.h file

---
+++ State.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "stdafx.h"
+#include "LinkedList.h"
+class State
+{
+public:
+	CBP callBack;
+	Bulb* callBackLocation;
+	State* next;
+	TStates state;
+	LinkedList<Transition>* transitions;
+
+	void Add(State* from, State* to, GP guard, Bulb* guardLocation);
+	State* CheckGuards(bool* changed);
+	void InvokeCallback();
+	bool operator!=(State& rhd);
+	bool operator==(State& rhd);
+	State(TStates state);
+	State(TStates state, CBP callBack, Bulb* callBackLocation);
+};
+
